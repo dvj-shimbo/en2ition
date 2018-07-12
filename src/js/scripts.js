6e9556476af9878dd016fcab4977b7613ed507a9
dvj-shimbo <dvj.shimbo@gmail.com>
2018-07-12 22:58:18 +0200

    Top Menu javascripts added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,49 @@
 $(window).on("load", function() {
 	"use strict";
 
-	// User's functions
+//	console.log("scripts.js is loaded");
 
-	console.log("scripts.js is loaded");
+	$("header").click(function(e) {
+
+		if ($(this).css("position") === "fixed") {
+
+			var posX = e.pageX;
+			var posY = e.pageY - $(window).scrollTop() + $(this).scrollTop();
+
+			var w = $(this).width() * 0.95;
+
+			var h = parseInt($(this).css("padding-top")) * 1.3;
+
+			if (posX  > w - 23 && posX < w && posY > h && posY < h + 24) {
+				if ($("body").hasClass("menu-active")) {
+					$(this).scrollTop(0);
+					$("body").removeClass("menu-active");
+				} else {
+					$("body").addClass("menu-active");
+				}
+			}
+
+		}
+	});
+
+
+	var headerSubMenuHide = function() {
+		$("header ul li ul.active").removeClass("active");
+	};
+
+	$("header ul li ul").mouseover(function() {
+		$("body").off("click", headerSubMenuHide);
+	});
+
+	$("header ul li ul").mouseout(function() {
+		$("body").on("click", headerSubMenuHide);
+	});
+
+	$("header ul li ul:not(.active)").click(function() {
+		$(this).addClass("active");
+	});
+
+
+
 
 });
